e318bdff7d9e43965c92bbc8f783a81dfe351a5e
MC <mc8@protonmail.com>
2023-10-26 13:17:16 -0700

    finished

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def get_weather(latitude, longitude):
     data = reponse.json()
     return data
 
+def send_text(body):
+    account_sid = "twilio_sid"
+    auth_token = "twilio_token"
+    from_number = "from_number"
+    to_number = "to_number"
+
+    client = Client(account_sid, auth_token)
+
+    message = client.message.create(
+        body = body,
+        from_ = from_number,
+        to_= to_number
+    )
+    print("SENT")
+
 def send_weather_update():
     # Weather to send
     latitude = 40.7128
@@ -14,9 +29,14 @@ def send_weather_update():
 
     weather_data = get_weather(latitude, longitude)
     temperature_celsius = weather_data["hourly"]["temperature_2m"][0]
-    relativehumidity = weather_data["hourly"]["relativehumidity_2m"][0]
-    windspeed =
+    relative_humidity = weather_data["hourly"]["relativehumidity_2m"][0]
+    wind_speed = weather_data["hourly"]["windspeed_10m"][0]
 
+    weather_info = (
+        f"temperature: {temperature_celsius:.2f}C\n"
+        f"relativehumidity: {relative_humidity}%\n"
+    )
+    send_text(weather_info)
 
 def main():
     sched.every().day.at("08:00").do(send_weather_update)
